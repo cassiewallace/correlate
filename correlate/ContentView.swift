e071a5272d98a3d591f626f0a9761c13222a3fb3
Cassie Wallace <cassbwallace@gmail.com>
2025-03-04 09:14:25 -0700

    Add basic Health data authorization

--- correlate/ContentView.swift
+++ correlate/ContentView.swift
@@ -5,56 +5,34 @@
 //  Created by Cassie Wallace on 3/4/25.
 //
 
+import HealthKitUI
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    @State var authenticated = false
+    @State var trigger = false
+    let healthStore = HKHealthStore()
+    
+    let allTypes: Set = [
+        HKQuantityType.stateOfMindType()
+    ]
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        Button("Access health data") {
+            if HKHealthStore.isHealthDataAvailable() {
+                trigger.toggle()
             }
-#if os(macOS)
-            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-#endif
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+        .disabled(!authenticated)
+        .healthDataAccessRequest(store: healthStore,
+                                 shareTypes: allTypes,
+                                 readTypes: allTypes,
+                                 trigger: trigger) { result in
+            switch result {
+            case .success(_):
+                authenticated = true
+            case .failure(let error):
+                fatalError("*** An error occurred while requesting authentication: \(error) ***")
             }
         }
     }
